ec8dd690d422b7f6e9f60f2452ec912795e85a72
xizila <154396234+xizila@users.noreply.github.com>
2023-12-23 23:03:18 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -10,7 +10,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('傻鱼,my sister|祝你|生日快乐|happy birthday|#countdown 3||');
+      S.UI.simulate('刘子锐,my Bosom friend|祝你|生日快乐|happy birthday|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
